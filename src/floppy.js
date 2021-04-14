6e19fe848c9850c3879cf4e753d3cdcf03aad6cd
Mikhail B <wproxym@at.gmail>
2021-04-14 11:02:13 +0300

    hotfix unknown floppy size to 1440 or 2880

--- src/floppy.js
+++ src/floppy.js
@@ -90,8 +90,9 @@ function FloppyController(cpu, fda_image, fdb_image)
         }
         else
         {
-            console.error("Unknown floppy size: " + h(fda_image.byteLength));
-            floppy_type = floppy_types[1440];
+            console.error("Unknown floppy size: " + h(this.floppy_size));
+            floppy_type = ((this.floppy_size >> 10) > 1440) ?  floppy_types[2880] : floppy_types[1440];
+            console.log("Floppy type rounded: " + (Object.keys(floppy_types)[Object.values(floppy_types).indexOf(floppy_type)]));
 
             cpu.devices.rtc.cmos_write(CMOS_FLOPPY_DRIVE_TYPE, floppy_type.type << 4);
 
@@ -100,6 +101,7 @@ function FloppyController(cpu, fda_image, fdb_image)
             number_of_cylinders = floppy_type.tracks;
 
         }
+        console.log("Floppy details: ", floppy_type);
 
         this.sectors_per_track = sectors_per_track;
         this.number_of_heads = number_of_heads;
